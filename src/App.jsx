ab758dcb1e2e7006404dd0f20adab22060e6f17c
TesloKnife <teslo@example.com>
2025-02-23 13:58:25 +0500

    Разбил проект на компоненты в папке src/handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { getButtons } from './get-buttons';
 import styles from './App.module.css';
 
 export const App = () => {
@@ -7,78 +8,18 @@ export const App = () => {
 	const [operand2, setOperand2] = useState('');
 	const [isResult, setIsResult] = useState(false);
 
-	const handleNum = (label) => {
-		if (operator === '') {
-			if (operand1 === '0') {
-				setOperand1(label);
-			} else {
-				setOperand1(operand1 + label);
-			}
-		} else {
-			if (operand2 === '0') {
-				setOperand2(label);
-			} else {
-				setOperand2(operand2 + label);
-			}
-		}
-		setIsResult(false);
+	const state = {
+		operand1,
+		setOperand1,
+		operator,
+		setOperator,
+		operand2,
+		setOperand2,
+		isResult,
+		setIsResult,
 	};
 
-	const handleReset = () => {
-		setOperand1('0');
-		setOperator('');
-		setOperand2('');
-		setIsResult(false);
-	};
-
-	const handlePlus = () => {
-		setOperator('+');
-		setIsResult(false);
-	};
-
-	const handleMinus = () => {
-		setOperator('-');
-		setIsResult(false);
-	};
-
-	const handleResult = () => {
-		if (operand2 !== '') {
-			switch (operator) {
-				case '+': {
-					setOperand1(Number(operand1) + Number(operand2));
-
-					break;
-				}
-				case '-': {
-					setOperand1(Number(operand1 - operand2));
-
-					break;
-				}
-				default:
-				// Ничего не делаем
-			}
-			setOperand2('');
-		}
-		setOperator('');
-		setIsResult(true);
-	};
-
-	const buttons = [
-		{ id: '1', label: '1', group: 'left', handler: handleNum },
-		{ id: '2', label: '2', group: 'left', handler: handleNum },
-		{ id: '3', label: '3', group: 'left', handler: handleNum },
-		{ id: '4', label: '4', group: 'left', handler: handleNum },
-		{ id: '5', label: '5', group: 'left', handler: handleNum },
-		{ id: '6', label: '6', group: 'left', handler: handleNum },
-		{ id: '7', label: '7', group: 'left', handler: handleNum },
-		{ id: '8', label: '8', group: 'left', handler: handleNum },
-		{ id: '9', label: '9', group: 'left', handler: handleNum },
-		{ id: '0', label: '0', group: 'left', handler: handleNum },
-		{ id: 'C', label: 'C', group: 'right', handler: handleReset },
-		{ id: '+', label: '+', group: 'right', handler: handlePlus },
-		{ id: '-', label: '-', group: 'right', handler: handleMinus },
-		{ id: '=', label: '=', group: 'right', handler: handleResult },
-	];
+	const buttons = getButtons(state);
 
 	const output = operand1 + operator + operand2;
 
